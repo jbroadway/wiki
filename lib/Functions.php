9ad07bfc561ca1b58c049173a514e23d319bd04d
Johnny Broadway <johnny@johnnybroadway.com>
2014-03-11 11:28:27 -0500

    Replaced /e in regex with preg_replace_callback()

--- lib/Functions.php
+++ lib/Functions.php
@@ -11,9 +11,11 @@ function wiki_parse_body ($body) {
  * Parse internal links of the form `[[Link name]]`.
  */
 function wiki_parse_links ($body) {
-	return preg_replace (
-		'/\[\[(.+?)\]\]/e',
-		'\'<a href="/wiki/\' . str_replace (\' \', \'-\', \'\\1\') . \'">\\1</a>\'',
+	return preg_replace_callback (
+		'/\[\[(.+?)\]\]/',
+		function ($m) {
+			return '<a href="/wiki/' . str_replace (' ', '-', $m[1]) . "\">$m[1]</a>";
+		},
 		$body
 	);
 }
@@ -33,9 +35,11 @@ function wiki_parse_html ($body) {
  * Add anchors to headers.
  */
 function wiki_parse_headers ($body) {
-	return preg_replace (
-		'/<h([1-6])>([^<\']+)/e',
-		'\'<a name="\' . strtolower (str_replace (\' \', \'-\', \'\\2\')) . \'"></a><h\1>\2\'',
+	return preg_replace_callback (
+		'/<h([1-6])>([^<\']+)/',
+		function ($m) {
+			return '<a name="' . strtolower (str_replace (' ', '-', $m[2])) . "\"></a><h$m[1]>$m[2]";
+		},
 		$body
 	);
 }
